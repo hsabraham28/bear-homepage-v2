44f7bfe6c9f00a38d151a04fa5bca7428bd62918
Hannah Abraham <hsabraham28@berkeley.edu>
2020-08-06 08:12:31 -0500

    Updates

---
+++ precache-manifest.ed6d8687eaa285c3231a705e13a01628.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b47bfe89f5523648569709e3c04cecf",
+    "url": "/bear-homepage-v2/index.html"
+  },
+  {
+    "revision": "d32e37cd24195bb6dbb9",
+    "url": "/bear-homepage-v2/static/css/main.563c1b20.chunk.css"
+  },
+  {
+    "revision": "773c1029197221a3d021",
+    "url": "/bear-homepage-v2/static/js/2.c596a8f2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bear-homepage-v2/static/js/2.c596a8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d32e37cd24195bb6dbb9",
+    "url": "/bear-homepage-v2/static/js/main.56d001aa.chunk.js"
+  },
+  {
+    "revision": "442a76d5af93cb2910c4",
+    "url": "/bear-homepage-v2/static/js/runtime-main.30aa8497.js"
+  },
+  {
+    "revision": "f801e234d51049bd538d3d95f6f4e110",
+    "url": "/bear-homepage-v2/static/media/GlacialIndifference-Bold.f801e234.otf"
+  },
+  {
+    "revision": "69b161f1f0d94f15afefbd34a37566d8",
+    "url": "/bear-homepage-v2/static/media/GlacialIndifference-Italic.69b161f1.otf"
+  },
+  {
+    "revision": "13dc531a2ffd9ca7eeb500515774fa74",
+    "url": "/bear-homepage-v2/static/media/GlacialIndifference-Regular.13dc531a.otf"
+  },
+  {
+    "revision": "cad4b3591ecdfe89ecf86ff8ebcba6e8",
+    "url": "/bear-homepage-v2/static/media/LeagueGothic-Regular.cad4b359.otf"
+  },
+  {
+    "revision": "2c300682d31727ebbad9b5481ffb3570",
+    "url": "/bear-homepage-v2/static/media/asuc-smaller.2c300682.png"
+  },
+  {
+    "revision": "3b327a9da0b2701f99f9c66d84d48e59",
+    "url": "/bear-homepage-v2/static/media/caltransparent.v3.cropped.3b327a9d.png"
+  },
+  {
+    "revision": "ff760e7099a6b3f916f871b341f8aee1",
+    "url": "/bear-homepage-v2/static/media/gradient.ff760e70.png"
+  },
+  {
+    "revision": "2a99f0e2bab9ae0ec4d0e7ea8c2d5946",
+    "url": "/bear-homepage-v2/static/media/homesStripes.2a99f0e2.PNG"
+  },
+  {
+    "revision": "48cf3224ac0f94fe8097dd46e8db1c7c",
+    "url": "/bear-homepage-v2/static/media/os-pin.48cf3224.png"
+  },
+  {
+    "revision": "8aa5cebec767d762f80225eae57b2d9f",
+    "url": "/bear-homepage-v2/static/media/vs-pin.8aa5cebe.png"
+  }
+]);

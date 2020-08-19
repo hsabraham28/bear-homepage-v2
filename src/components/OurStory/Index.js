301f95d40092c3315346af0179053d750ae54ff2
William Louis <wlouis23@berkeley.edu>
2020-08-19 14:14:43 -0700

    Added the REST endpoint for OurStory

--- src/components/OurStory/Index.js
+++ src/components/OurStory/Index.js
@@ -8,38 +8,14 @@ import ourStory from './Tile';
 function Index() {
     // Placeholder for REST API; remove contents when implementing back-end.
     const [state, setState] = useState({
-        stories: [
-            {
-                name: "name1",
-                story: 'story1 Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'
-            },
-            {
-                name: 'name2',
-                story: 'story1 Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'
-            },
-            {
-                name: 'name3',
-                story: 'story1 Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'
-            },
-            {
-                name: 'name4',
-                story: 'story1 Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'
-            },
-            {
-                name: 'name5',
-                story: 'story1 Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'
-            },
-            {
-                name: 'name6',
-                story: 'story1 Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'
-            },
-            {
-                name: 'name7',
-                story: 'story1 Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'
-            },
-        ]
+        stories: []
     })
 
+    fetch(`http://localhost:8083`)
+    .then(response => response.json())
+    .then(result =>  {
+        setState({stories: result})
+    })
     return (
         <div style={main}>
             <Sidebar />

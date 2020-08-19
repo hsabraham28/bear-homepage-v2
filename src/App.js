223a0c8ca2447592caa26c0eb203c9d702151152
Ivan Alexander Kristanto <ivalexander13@berkeley.edu>
2020-08-19 15:19:30 -0700

    fix our story tile widths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import './Assets/scss/default.scss';
 import './Assets/scss/shared.scss';
 import { HashRouter as Router, Route} from 'react-router-dom';
@@ -8,8 +8,6 @@ import Header from './components/Header/Header';
 import Landing from './components/Landing/Landing';
 import VirtualSproul from './components/VirtualSproul/VirtualSproul';
 import Story from './components/OurStory/Index';
-
-import $ from 'jquery';
 
 
 function App() {

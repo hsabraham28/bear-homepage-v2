5923d0e725cfcb9b109cd224b85e6a5196fbcce9
Ivan Alexander Kristanto <ivalexander13@berkeley.edu>
2020-08-21 11:15:50 -0700

    fixed modal id and props

--- src/components/VirtualSproul/resultComponent/clubCardComponent/clubCard.js
+++ src/components/VirtualSproul/resultComponent/clubCardComponent/clubCard.js
@@ -6,9 +6,7 @@ import oski from './oski-big.png';
 //console.log(oski);
 
 
-
-
-class ClubCard extends Component{
+class ClubCard extends Component {
   constructor(props) {
     super()
   }
@@ -26,7 +24,6 @@ class ClubCard extends Component{
 
 
       <div className="clubLayout">
-        
 
         <MDBCard wide cascade data-toggle="modal" data-target={cardIDPound}>
           <MDBView cascade>
@@ -39,8 +36,6 @@ class ClubCard extends Component{
             />
           </MDBView>
 
-
-
           <MDBCardBody cascade className='text-center'>
             <MDBCardTitle className='card-title'>
               <strong>{org.name}</strong>
@@ -48,74 +43,59 @@ class ClubCard extends Component{
 
 
             <MDBCardText>
-              
-            </MDBCardText>
 
+            </MDBCardText>
             <MDBCol md='12' className='d-flex justify-content-center'>
-              
             </MDBCol>
           </MDBCardBody>
-
         </MDBCard>
-     
-      <div className="modal fade" id={cardID} tabindex="-1" role="dialog" aria-labelledby={titleID} aria-hidden="true">
-  <div className="modal-dialog" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
 
+        <div className="modal fade" id={cardID} tabindex="-1" role="dialog" aria-labelledby={titleID} aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id={titleID}>{org.name}</h5>
 
-        <h5 className="modal-title" id={titleID}>{org.name}</h5>
-        
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-     
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
 
-      <div className="modal-body">
-
-
-        <div className="container-fluid">
-        <div className="row">
-      <div className="col-md-3">Email: </div>
-      <div className="col-md-9 ml-auto">{org.clubEmail}</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">Size: </div>
-      <div className="col-md-9 ml-auto">{org.size}</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">What is the student organization about? </div>
-      <div className="col-md-9 ml-auto">{org.intro}</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">What is it like to be a part of the student organization? </div>
-    <div className="col-md-9 ml-auto">{org.experience}</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">Tags </div>
-      <div className="col-md-9 ml-auto">{org.tags}</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">Social Media: </div>
-      <div className="col-md-9 ml-auto">{org.socialMedia}</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">Youtube Link: </div>
-      <div className="col-md-9 ml-auto">{org.yTLink}</div>
-    </div>
-    
-
-  </div>
-
-
-      </div>
-      
-    </div>
-  </div>
-</div>
-
-  </div>
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-3">Email: </div>
+                    <div className="col-md-9 ml-auto">{org.clubEmail}</div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-3">Size: </div>
+                    <div className="col-md-9 ml-auto">{org.size}</div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-3">What is the student organization about? </div>
+                    <div className="col-md-9 ml-auto">{org.intro}</div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-3">What is it like to be a part of the student organization? </div>
+                    <div className="col-md-9 ml-auto">{org.experience}</div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-3">Tags </div>
+                    <div className="col-md-9 ml-auto">{org.tags}</div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-3">Social Media: </div>
+                    <div className="col-md-9 ml-auto">{org.socialMedia}</div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-3">Youtube Link: </div>
+                    <div className="col-md-9 ml-auto">{org.yTLink}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
   );
   }
 

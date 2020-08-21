99184e07b02a5877b2ad3c63ef34136a3ea7c588
Ivan Alexander Kristanto <ivalexander13@berkeley.edu>
2020-08-21 11:15:50 -0700

    fixed modal id and props

--- src/components/VirtualSproul/resultComponent/clubCardComponent/clubCard.js
+++ src/components/VirtualSproul/resultComponent/clubCardComponent/clubCard.js
@@ -6,103 +6,106 @@ import oski from './oski-big.png';
 console.log(oski);
 
 
+function ClubCard(props) {
 
-
-function ClubCard(props){
+  const cardIDPound = `#a` + props.org.id;
+  const cardID = `a` + props.org.id;
   
-    return (
+  const titleID = 'title' + props.org.id;
 
-      <div className="clubLayout">
-        
+  return (
 
-        <MDBCard wide cascade data-toggle="modal" data-target="#exampleModalLong">
-          <MDBView cascade>
-            <MDBCardImage
-              hover
-              overlay='white-slight'
-              className='card-img-top'
-              src={oski}
-              alt='Card cap'
-            />
-          </MDBView>
+    <div className="clubLayout">
 
 
-
-          <MDBCardBody cascade className='text-center'>
-            <MDBCardTitle className='card-title'>
-              <strong>{props.name}</strong>
-            </MDBCardTitle>
-
-
-            <MDBCardText>
-              {props.info}
-            </MDBCardText>
-
-            <MDBCol md='12' className='d-flex justify-content-center'>
-              
-            </MDBCol>
-          </MDBCardBody>
-
-        </MDBCard>
-     
-      <div className="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div className="modal-dialog" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
+      <MDBCard wide cascade data-toggle="modal" data-target={cardIDPound}>
+        <MDBView cascade>
+          <MDBCardImage
+            hover
+            overlay='white-slight'
+            className='card-img-top'
+            src={oski}
+            alt='Card cap'
+          />
+        </MDBView>
 
 
-        <h5 className="modal-title" id="exampleModalLongTitle">Name of Student Organization</h5>
-        
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+
+        <MDBCardBody cascade className='text-center'>
+          <MDBCardTitle className='card-title'>
+            <strong>{props.org.name}</strong>
+          </MDBCardTitle>
+
+
+          <MDBCardText>
+            {props.org.info}
+          </MDBCardText>
+
+          <MDBCol md='12' className='d-flex justify-content-center'>
+
+          </MDBCol>
+        </MDBCardBody>
+
+      </MDBCard>
+
+      <div className="modal fade" id={cardID} tabIndex="-1" role="dialog" aria-labelledby={titleID} aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+
+
+              <h5 className="modal-title" id={titleID}>Name of Student Organization</h5>
+
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+
+            <div className="modal-body">
+
+
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-3">{props.org.name}</div>
+                  <div className="col-md-9 ml-auto">Email goes here</div>
+                </div>
+                <div className="row">
+                  <div className="col-md-3">Size: </div>
+                  <div className="col-md-9 ml-auto">Size goes here</div>
+                </div>
+                <div className="row">
+                  <div className="col-md-3">What is the student organization about? </div>
+                  <div className="col-md-9 ml-auto">Description goes here</div>
+                </div>
+                <div className="row">
+                  <div className="col-md-3">What is it like to be a part of the student organization? </div>
+                  <div className="col-md-9 ml-auto">Answer goes here</div>
+                </div>
+                <div className="row">
+                  <div className="col-md-3">Tags </div>
+                  <div className="col-md-9 ml-auto">Tags go here</div>
+                </div>
+                <div className="row">
+                  <div className="col-md-3">Social Media: </div>
+                  <div className="col-md-9 ml-auto">Social Media goes here</div>
+                </div>
+                <div className="row">
+                  <div className="col-md-3">Youtube Link: </div>
+                  <div className="col-md-9 ml-auto">YT Link goes here</div>
+                </div>
+
+
+              </div>
+
+
+            </div>
+
+          </div>
+        </div>
       </div>
-     
 
-      <div className="modal-body">
-
-
-        <div className="container-fluid">
-        <div className="row">
-      <div className="col-md-3">Email: </div>
-      <div className="col-md-9 ml-auto">Email goes here</div>
     </div>
-    <div className="row">
-      <div className="col-md-3">Size: </div>
-      <div className="col-md-9 ml-auto">Size goes here</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">What is the student organization about? </div>
-      <div className="col-md-9 ml-auto">Description goes here</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">What is it like to be a part of the student organization? </div>
-      <div className="col-md-9 ml-auto">Answer goes here</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">Tags </div>
-      <div className="col-md-9 ml-auto">Tags go here</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">Social Media: </div>
-      <div className="col-md-9 ml-auto">Social Media goes here</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3">Youtube Link: </div>
-      <div className="col-md-9 ml-auto">YT Link goes here</div>
-    </div>
-    
-
-  </div>
-
-
-      </div>
-      
-    </div>
-  </div>
-</div>
-
-  </div>
   );
 }
 

38ca6c5bf9392c79a2585e0b33c60804daf8d738
Ivan Alexander Kristanto <ivalexander13@berkeley.edu>
2020-08-22 15:47:02 -0700

    finish about page, needs data

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -34,6 +34,24 @@ function About(props) {
                 pic: placeholder
             },
         ],
+        uiux: [
+            {
+                name: "Person1",
+                pic: placeholder,
+            },
+        ],
+        marketing: [
+            {
+                name: "person2",
+                pic: placeholder,
+            }
+        ],
+        special: [
+            {
+                name: "person1",
+                pic: placeholder
+            }
+        ]
     }
 
     return (
@@ -41,9 +59,11 @@ function About(props) {
             <Header isLanding={false} />
             <div className="about-main">
                 <div className="heading">
-                    <h2>THE STORY OF BEAR BEGINNINGS</h2>
-                    <div className="storybox">
-                        <p>Add a paragraph about why we started Bear Beginnings and the story behind it.</p>
+                    <div className="heading-overlay">
+                        <h2>THE STORY OF BEAR BEGINNINGS</h2>
+                        <div className="storybox">
+                            <p>Add a paragraph about why we started Bear Beginnings and the story behind it.</p>
+                        </div>
                     </div>
                 </div>
                 <div class="body">
@@ -52,6 +72,12 @@ function About(props) {
                     <Team team={teams.frontend}/>
                     <h3>Back End Team</h3>
                     <Team team={teams.backend}/>
+                    <h3>Marketing Team</h3>
+                    <Team team={teams.marketing}/>
+                    <h3>UI/UX Team</h3>
+                    <Team team={teams.uiux}/>
+                    <h3>Special Thanks</h3>
+                    <Team team={teams.special}/>
 
                     
                 </div>
--- src/components/About/Member.js
+++ src/components/About/Member.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from 'react';
+import mask from './about-mask.png';
 
-function Members(props) {
-    return (
-        <div className="member">
-            <div>
-                <img src={props.pic}></img>
+
+function Member(props) {
+    const memberImageStyle = {
+        backgroundImage: `url(${props.pic})`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+
+        WebkitMaskImage: `url(${mask})`,
+        WebkitMaskSize: 'contain',
+        WebkitMaskRepeat: 'no-repeat',
+        WebkitMaskPosition: 'center',
+
+        maskImage: `url(${mask})`,
+        maskSize: 'contain',
+        maskRepeat: 'no-repeat',
+        maskPosition: 'center'
+    };
+    return(<div className="member">
+            <div style={memberImageStyle}>
+                <img src={props.pic} />
             </div>
             <h2>{props.name}</h2>
-        </div>
-    )
+        </div>);
 }
 
-export default Members
+export default Member;

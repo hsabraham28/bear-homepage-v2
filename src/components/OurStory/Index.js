b9840a647e9736525914716daa9cf3a2a743bf14
Ivan Alexander Kristanto <ivalexander13@berkeley.edu>
2020-08-31 00:41:16 -0700

    comment out placeholder data

--- src/components/OurStory/Index.js
+++ src/components/OurStory/Index.js
@@ -12,63 +12,63 @@ class Index extends React.Component {
         super()
         this.state = {
             stories: [
-                { // base: required fields
-                    id: "id1", //whatever works
-                    name: "name1",
-                    year: "year1", // string (freshmen, junior, etc)
-                    major: "major1",
-                    storyType: "academics", // string (either "academics", "transition", "student orgs") 
+                // { // base: required fields
+                //     id: "id1", //whatever works
+                //     name: "name1",
+                //     year: "year1", // string (freshmen, junior, etc)
+                //     major: "major1",
+                //     storyType: "academics", // string (either "academics", "transition", "student orgs") 
 
-                    // academics
-                    email: "email1",
-                    college: "college1",
-                    gpareq: "gpareq1",
-                    prereq: "prereq1",
-                    tips: "tips1",
-                    rigor: "rigor1",
-                },
-                { // base: required fields
-                    id: "id2", //whatever works
-                    name: "name1",
-                    year: "year1", // string (freshmen, junior, etc)
-                    major: "major1",
-                    storyType: "transition", // string (either "academics", "transition", "clubs", "apps") 
+                //     // academics
+                //     email: "email1",
+                //     college: "college1",
+                //     gpareq: "gpareq1",
+                //     prereq: "prereq1",
+                //     tips: "tips1",
+                //     rigor: "rigor1",
+                // },
+                // { // base: required fields
+                //     id: "id2", //whatever works
+                //     name: "name1",
+                //     year: "year1", // string (freshmen, junior, etc)
+                //     major: "major1",
+                //     storyType: "transition", // string (either "academics", "transition", "clubs", "apps") 
 
-                    // transition
-                    residency: "in-state", // either "in-state", "out-of-state", or "international"
-                    feel: "feel1",
-                    emo: "emo1",
-                    concern: "concern1",
-                    challenge: "challenge1",
-                },
-                { // base: required fields
-                    id: "id3", //whatever works
-                    name: "name1",
-                    year: "year1", // string (freshmen, junior, etc)
-                    major: "major1",
-                    storyType: "student orgs", // string (either "academics", "transition", "clubs", "apps") 
+                //     // transition
+                //     residency: "in-state", // either "in-state", "out-of-state", or "international"
+                //     feel: "feel1",
+                //     emo: "emo1",
+                //     concern: "concern1",
+                //     challenge: "challenge1",
+                // },
+                // { // base: required fields
+                //     id: "id3", //whatever works
+                //     name: "name1",
+                //     year: "year1", // string (freshmen, junior, etc)
+                //     major: "major1",
+                //     storyType: "student orgs", // string (either "academics", "transition", "clubs", "apps") 
 
-                    // club
-                    clubs: "clubs1",
-                    transition: "transition1",
-                    proscons: "proscons1",
-                    balance: "balance1",
-                    type: "clubs",
-                },
-                { // base: required fields
-                    id: "id4", //whatever works
-                    name: "name1",
-                    year: "year1", // string (freshmen, junior, etc)
-                    major: "major1",
-                    storyType: "student orgs", // string (either "academics", "transition", "clubs", "apps") 
+                //     // club
+                //     clubs: "clubs1",
+                //     transition: "transition1",
+                //     proscons: "proscons1",
+                //     balance: "balance1",
+                //     type: "clubs",
+                // },
+                // { // base: required fields
+                //     id: "id4", //whatever works
+                //     name: "name1",
+                //     year: "year1", // string (freshmen, junior, etc)
+                //     major: "major1",
+                //     storyType: "student orgs", // string (either "academics", "transition", "clubs", "apps") 
 
-                    // app
-                    factors: "factors1",
-                    cope: "cope1",
-                    advice: "advice1",
-                    pic: "pic1",
-                    type: "apps",
-                },
+                //     // app
+                //     factors: "factors1",
+                //     cope: "cope1",
+                //     advice: "advice1",
+                //     pic: "pic1",
+                //     type: "apps",
+                // },
             ]
         }
 
@@ -81,7 +81,7 @@ class Index extends React.Component {
         fetch(`http://157.245.228.180:8083`)
             .then(response => response.json())
             .then(result => {
-                this.setState({ stories: result }, () => console.log(this.state.stories))
+                this.setState({ stories: result }) //for debug use: , () => console.log(this.state.stories)
             })
     }
 

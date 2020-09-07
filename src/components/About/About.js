428593f176adc2f70aef91951fbd3a270ce6c705
Ivan Alexander Kristanto <ivalexander13@berkeley.edu>
2020-09-07 16:55:26 -0700

    added people

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,12 +17,16 @@ function About(props) {
         ],
         backend: [
             {
-                name: "",
-                pic: require('./teampics/placeholder.png')
+                name: "William Louis",
+                pic: require('./teampics/william.jpeg')
             },
             {
                 name: "Muse Yang",
                 pic: require('./teampics/muse.jpg')
+            },
+            {
+                name: "Ruirui Zhang",
+                pic: require('./teampics/ruirui.jpg')
             },
         ],
         uiux: [
@@ -31,24 +35,34 @@ function About(props) {
                 pic: require("./teampics/muskaan.jpg"),
             },
             {
-                name: "Danji Liu",
-                pic: require("./teampics/danji.jpg"),
-            },
-            {
                 name: "Yuqi Ye",
                 pic: require("./teampics/yuqi.jpg"),
+            },
+            {
+                name: "Wing Yiu Lo",
+                pic: require("./teampics/placeholder.png"),
             },
         ],
         marketing: [
             {
                 name: "Arrushi Agarwal",
                 pic: require('./teampics/arrushi.jpg'),
-            }
+            },
+            {
+                name: "Michael Paller",
+                pic: require('./teampics/michael.png'),
+            },
         ],
         special: [
             {
-                name: "person1",
-                pic: require('./teampics/placeholder.png')
+                name: "Danji Liu",
+                pic: require("./teampics/danji.jpg"),
+            },
+        ],
+        pm: [
+            {
+                name: "Sreeja Apparaju",
+                pic: require("./teampics/sreeja.jpeg"),
             }
         ]
     }
@@ -67,6 +81,8 @@ function About(props) {
                 </div>
                 <div class="body">
                     <h2>MEET THE TEAM</h2>
+                    <h3>Project Manager</h3>
+                    <Team team={teams.pm} />
                     <h3>UI/UX Team</h3>
                     <Team team={teams.uiux} />
                     <h3>Front End Team</h3>

24174b55618a27c3fc4d35f66d9c034b2cab76db
bluepegasus123 <bluepegasus123@gmail.com>
2020-09-20 23:16:29 -0700

    Added links to the react page for Developer applications and our story entries

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -19,9 +19,18 @@ function Contact() {
                     <a className="join" href="https://forms.gle/Eqe4xxiyo18RPRcs9">
                         <h2>Register your student organization here! </h2>
                     </a>
+
+                    <a className="join" href="https://docs.google.com/document/d/1yOJpkO7cSE7miOK5_7hvT0h9FYdm2pebYmUIYFDCzKk/edit?usp=sharing">
+                        <h2>Add your story here! </h2>
+                    </a>
+
                     <h4>Have any thoughts, questions, or concerns?</h4>
                     <a className="email" href="mailto:bearbeginningsberkeley@gmail.com">
                         <h4>Contact us through email!</h4>
+                    </a>
+
+                    <a className="email" href="https://forms.gle/HJTUXYXtH4N9FSDe8">
+                        <h4>Apply to our developer team!</h4>
                     </a>
                 </div>
             </div>
